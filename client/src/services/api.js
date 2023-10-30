7c82d64a61eb72f7fb55575274b7c149d166145f
Smit Suthar <smit@goswirl.live>
2023-10-30 11:19:13 +0530

    backend api changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rich-rose-antelope-suit.cyclic.app', // Replace with your server's URL
+  baseURL: 'https://rich-rose-antelope-suit.cyclic.app/api', // Replace with your server's URL
 });
 export const registerUser = async (userData) => {
   try {
